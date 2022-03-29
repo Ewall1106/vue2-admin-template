398ad1d8d7a25d709f0542db3b50124ce0cdcb78
xiongwei <>
2022-03-29 19:29:09 +0800

    add

--- src/components/MenuMultiple/index.vue
+++ src/components/MenuMultiple/index.vue
@@ -1,57 +1,62 @@
 <template>
   <div class="menu-multiple">
     <div style="width: 256px; flex-shrink: 0; border-right: 1px solid #e6e6eb">
-      <el-menu :default-openeds="defaultOpeneds" @open="handleOpen" @close="handleClose">
-        <el-submenu index="1" class="submenu-level-1">
-          <template slot="title">
-            <div class="submenu-level-1-title">
-              <h3>触发节点1</h3>
-              <div style="display: flex; align-items: center">
-                <i class="el-icon-document-copy icon"></i>
-                <i class="el-icon-delete icon"></i>
+      <!-- level 1 -->
+      <div class="submenu" v-for="(i, idx) in menuList" :key="idx">
+        <div
+          class="submenu-level-1"
+          :class="i.isActive ? 'subMenuActive' : ''"
+          @click="handleMenuClick(i, idx)"
+        >
+          <div>
+            <i
+              class="el-icon-arrow-down iconArrow"
+              :class="i.isExpand ? 'iconExpand' : 'iconCollapse'"
+            />
+            <span style="margin-left: 8px">{{ i.label }}</span>
+          </div>
+          <div style="display: flex; align-items: center">
+            <i class="el-icon-copy-document icon"></i>
+            <i class="el-icon-delete icon"></i>
+          </div>
+        </div>
+        <!-- level 2 -->
+        <el-collapse-transition>
+          <div v-show="i.isExpand">
+            <div v-for="(j, idx) in i.children" :key="idx">
+              <div class="submenu-level-2" @click="handleMenuClick(j, idx)">
+                <i
+                  class="el-icon-arrow-down iconArrow"
+                  :class="j.isExpand ? 'iconExpand' : 'iconCollapse'"
+                />
+                <span style="margin-left: 8px">{{ j.label }}</span>
               </div>
+              <!-- level 3 -->
+              <el-collapse-transition>
+                <div v-show="j.isExpand">
+                  <div
+                    class="submenu-level-3"
+                    :class="k.isActive ? 'subMenuActive' : ''"
+                    v-for="(k, idx) in j.children"
+                    :key="idx"
+                    @click="handleMenuClick(k)"
+                  >
+                    <div style="display: flex; align-items: center">
+                      <i class="el-icon-menu icon"></i>
+                      <span style="margin-left: 4px"> {{ k.label }}</span>
+                    </div>
+                    <div style="display: flex; align-items: center">
+                      <i class="el-icon-plus icon" @click.stop="handleOpen"></i>
+                      <i class="el-icon-copy-document icon"></i>
+                      <i class="el-icon-delete icon"></i>
+                    </div>
+                  </div>
+                </div>
+              </el-collapse-transition>
             </div>
-          </template>
-
-          <el-submenu index="1-1" class="submenu-level-2">
-            <template slot="title">
-              <div class="submenu-level-2-title">默认分组1</div>
-            </template>
-            <el-menu-item index="1-1-1" class="submenu-level-3">
-              <div class="content">
-                <div style="display: flex; align-items: center">
-                  <i class="el-icon-menu icon"></i>
-                  <span>规则1</span>
-                </div>
-                <div style="display: flex; align-items: center">
-                  <i class="el-icon-plus icon" @click.stop="handleOpen"></i>
-                  <i class="el-icon-document-copy icon"></i>
-                  <i class="el-icon-delete icon"></i>
-                </div>
-              </div>
-            </el-menu-item>
-          </el-submenu>
-
-          <el-submenu index="1-2" class="submenu-level-2">
-            <template slot="title">
-              <div class="submenu-level-2-title">默认分组2</div>
-            </template>
-            <el-menu-item index="1-2-1" class="submenu-level-3">
-              <div class="content">
-                <div style="display: flex; align-items: center">
-                  <i class="el-icon-menu icon"></i>
-                  <span>规则1</span>
-                </div>
-                <div style="display: flex; align-items: center">
-                  <i class="el-icon-plus icon" @click.stop="handleOpen"></i>
-                  <i class="el-icon-document-copy icon"></i>
-                  <i class="el-icon-delete icon"></i>
-                </div>
-              </div>
-            </el-menu-item>
-          </el-submenu>
-        </el-submenu>
-      </el-menu>
+          </div>
+        </el-collapse-transition>
+      </div>
     </div>
     <div style="flex: 1">2</div>
   </div>
@@ -62,26 +67,63 @@ export default {
   name: "MenuMultiple",
   data() {
     return {
-      defaultOpeneds: ["1", "1-1", "1-1-1"],
+      menuList: [
+        {
+          id: 1, // 必须保证id是唯一的
+          label: "触发节点1",
+          isExpand: true,
+          isActive: false,
+          children: [
+            {
+              id: 2,
+              label: "默认分组1",
+              isExpand: true,
+              isActive: false,
+              children: [
+                {
+                  id: 3,
+                  label: "规则1",
+                  isExpand: true,
+                  isActive: false,
+                },
+                {
+                  id: 4,
+                  label: "规则2",
+                  isExpand: true,
+                  isActive: false,
+                },
+              ],
+            },
+          ],
+        },
+      ],
     };
   },
   methods: {
-    handleOpen() {},
-    handleClose() {},
+    handleMenuClick(item) {
+      item.isExpand = !item.isExpand;
+
+      this.updateTree();
+      item.isActive = true;
+    },
+    updateTree() {
+      const dfs = (item) => {
+        if (item.children && item.children.length) {
+          for (let i = 0; i < item.children.length; i++) {
+            const child = item.children[i];
+            child.isActive = false;
+            dfs(child);
+          }
+        }
+      };
+      for (let item of this.menuList) {
+        item.isActive = false;
+        dfs(item);
+      }
+    },
   },
 };
 </script>
-
-<style>
-.menu-multiple .submenu-level-1 .el-submenu__icon-arrow {
-  left: 20px !important;
-  right: auto !important;
-}
-.menu-multiple .submenu-level-2 .el-submenu__icon-arrow {
-  left: 40px !important;
-  right: auto !important;
-}
-</style>
 
 <style lang="scss" scoped>
 .menu-multiple {
@@ -90,41 +132,73 @@ export default {
   border-radius: 8px;
   box-shadow: 0 2px 12px 0 #ebeef5;
 
-  .submenu-level-1-title {
-    box-sizing: border-box;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding-left: 20px;
-    font-size: 16px;
-    font-family: PingFangSC-Medium;
+  .subMenuActive {
+    color: #6c6cff !important;
+    background: #f0f0ff;
   }
 
-  .submenu-level-2-title {
-    padding-left: 20px;
+  .submenu-level-1 {
+    cursor: pointer;
+    height: 36px;
+    padding: 0 18px;
+    display: flex;
+    color: #333;
+    font-size: 16px;
+    font-family: PingFangSC-Medium;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .submenu-level-2 {
+    cursor: default;
+    display: flex;
+    align-items: center;
+    height: 36px;
+    padding: 0 40px;
+    color: #333;
     font-size: 14px;
     font-family: PingFangSC-Medium;
-    color: #3d3e40;
   }
 
   .submenu-level-3 {
-    padding-right: 20px;
-    .content {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      color: #666666;
-    }
+    cursor: pointer;
+    padding: 0 20px 0 56px;
+    height: 36px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: #666666;
+    font-family: PingFangSC-Regular;
+    font-size: 14px;
+  }
+
+  .submenu-level-1:hover,
+  .submenu-level-2:hover,
+  .submenu-level-3:hover {
+    background: #f0f0ff8a;
   }
 }
 
-.menu-multiple .icon {
+.icon {
   font-size: 15px;
-  margin-right: 0;
-  /* cursor: default; */
+  margin-left: 8px;
 }
 
-.menu-multiple .icon:hover {
-  color: red;
+.icon:hover {
+  color: #6c6cff;
+}
+
+.iconArrow {
+  font-size: 14px;
+  color: #ccc;
+  transition: transform 0.2s;
+}
+
+.iconExpand {
+  transform: rotate(0);
+}
+
+.iconCollapse {
+  transform: rotate(-90deg);
 }
 </style>
